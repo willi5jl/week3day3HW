72df2b871383cea9676042aea26eb25331754f5f
Jenna <jennnawilliams513@gmail.com>
2022-01-19 22:58:05 -0500

    hw

---
+++ app.js
@@ -0,0 +1,24 @@
+var a = 4;
+var b = 5;
+var c = 10;
+var d = 20;
+var e = 30;
+var f = 40;
+var g = 50;
+var h = 60;
+var i = 70;
+var j = 80;
+console.log(e+g)
+console.log(d+e)
+console.log(j-i)
+console.log(e-a)
+console.log(c*d)
+console.log(g*h)
+console.log(g/c)
+console.log(j/c)
+
+f++
+g++
+
+b--
+j--
